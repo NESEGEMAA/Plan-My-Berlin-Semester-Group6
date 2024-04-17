0be207bc9e1469d09dffe146e70e829fbb4def69
nesegemaa <Xxnour998xX@gmail.com>
2024-04-17 15:12:31 +0200

    Added defined Data Structures

--- Team_6.pl
+++ Team_6.pl
@@ -2,3 +2,10 @@
 % instead of needing to consult the files individually.
 % This line MUST be included in the final submission.
 :- ['transport_kb', 'slots_kb'].
+
+% Data Structures
+day_timing(Week, Day).
+
+journey(Week_Num, Week_Day, Start_Hour, Start_Minute, Total_Duration, Routes).
+
+route(Line, Start_Station, End_Station, Duration).
