e7ab8699331cc3f647f605f1de39eebc1fb60850
nesegemaa <Xxnour998xX@gmail.com>
2024-04-29 21:37:58 +0200

    implemented the group_days predicate which returns the "day_timing"s for a given group, needed a remove duplicates predicate which was implemented as well

--- Team_6.pl
+++ Team_6.pl
@@ -7,3 +7,20 @@
 % day_timing(Week, Day).
 % journey(Week_Num, Week_Day, Start_Hour, Start_Minute, Total_Duration, Routes).
 % route(Line, Start_Station, End_Station, Duration).
+
+% Predicates
+% SLOTS
+group_days(Group, Day_Timings):-
+    X = day_timing(Week, Day),
+    findall(X, scheduled_slot(Week, Day, _, _, Group), CrudeResult),
+    remove_duplicates(CrudeResult, Day_Timings).
+
+% General Helper Methods
+remove_duplicates([], []).
+
+remove_duplicates([Head | Tail], Result) :-
+    member(Head, Tail), !,
+    remove_duplicates(Tail, Result).
+
+remove_duplicates([Head | Tail], [Head | Result]) :-
+    remove_duplicates(Tail, Result).
