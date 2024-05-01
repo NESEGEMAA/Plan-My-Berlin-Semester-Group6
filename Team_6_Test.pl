172dc57d5a036fe9e68f34f90b4a7b23ec0d31e3
Omar Safwat <omarsafsab2004@gmail.com>
2024-05-01 18:51:50 +0200

    finished connected and started on travel_plan

--- Team_6_Test.pl
+++ Team_6_Test.pl
@@ -36,8 +36,14 @@ earliest_slot(Group, Week, Day, Slot):-
     Slot is H.
 
 %implementing Transportation -> proper_connection/4
-
 proper_connection(Station_A, Station_B, Duration, Line):-
+    (var(Line) -> line(Line,_),
+    proper_connection_helper(Station_A,Station_B,Duration,Line)
+    ;
+    proper_connection_helper(Station_A,Station_B,Duration,Line)
+    ).
+
+proper_connection_helper(Station_A, Station_B, Duration, Line):-
     connection(Station_A,_,_,Line),
     (var(Station_B)
     -> proper_connection_forwards(Station_A, Station_B, Duration, Line, [Station_A], 0)
@@ -47,9 +53,8 @@ proper_connection(Station_A, Station_B, Duration, Line):-
     ),
     Duration \== 0.
 
-proper_connection(Station_A, Station_B, Duration, Line):-
+proper_connection_helper(Station_A, Station_B, Duration, Line):-
     \+unidirectional(Line),
-    connection(Station_A,_,_,Line),
     (var(Station_B)
     -> proper_connection_backwards(Station_A, Station_B, Duration, Line, [Station_A], 0)
     ; 
@@ -63,7 +68,7 @@ proper_connection_forwards(Station_A, Station_B, Duration, Line, PreviouslyVisit
     connection(Station_A, X, Duration1, Line),
     (member(X, PreviouslyVisited) -> Duration is 0
     ;
-    (append([X], PreviouslyVisited, PreviouslyVisited1),
+    (append(PreviouslyVisited, [X], PreviouslyVisited1),
     DurationAcc1 is DurationAcc + Duration1,
     proper_connection_forwards(X, Station_B, Duration, Line, PreviouslyVisited1, DurationAcc1))).
 
@@ -72,7 +77,7 @@ proper_connection_backwards(Station_A, Station_B, Duration, Line, PreviouslyVisi
     connection(X, Station_A, Duration1, Line),
     (member(X, PreviouslyVisited) ->  Duration is 0
     ;
-    (append([X], PreviouslyVisited, PreviouslyVisited1),
+    (append(PreviouslyVisited, [X], PreviouslyVisited1),
     DurationAcc1 is DurationAcc + Duration1,
     proper_connection_backwards(X, Station_B, Duration, Line, PreviouslyVisited1, DurationAcc1))).
     
@@ -83,6 +88,8 @@ append_connection(Conn_Source,Conn_Destination,Conn_Duration,Conn_Line,[],[route
 append_connection(Conn_Source,Conn_Destination,Conn_Duration,Conn_Line,Routes_So_Far,Routes):-
     last(Routes_So_Far, E),
     E = route(Line, Source, Destination, Duration),
+    not(member(route(Conn_Line, Conn_Source, Conn_Destination, Conn_Duration), Routes_So_Far)),
+    Source \== Conn_Destination,
     Destination == Conn_Source,
     Line == Conn_Line,
     !,
@@ -91,6 +98,10 @@ append_connection(Conn_Source,Conn_Destination,Conn_Duration,Conn_Line,Routes_So
     append(Corrected_Routes_So_Far,[route(Line, Source, Conn_Destination, Correct_Duration)],Routes).
 
 append_connection(Conn_Source, Conn_Destination, Conn_Duration, Conn_Line, Routes_So_Far, Routes):-
+    last(Routes_So_Far, E),
+    E = route(_, Source, _, _),
+    not(member(route(Conn_Line, Conn_Source, Conn_Destination, Conn_Duration), Routes_So_Far)),
+    Source \== Conn_Destination,
     append(Routes_So_Far, [route(Conn_Line, Conn_Source, Conn_Destination, Conn_Duration)], Routes).
 
 %DeleteLast Predicate
@@ -102,22 +113,167 @@ delete_last([H|[]], []):-
 delete_last([H|T], [H|List]):-  
     delete_last(T, List).
 
-%implementing Transportation -> connected/8
-
+%implementing Transportation -> connected/8 and Transportation -> connected/10
 connected(Source, Destination, Week, Day, Max_Duration, Max_Routes, Duration, Routes):-
-    connected_helper(Source, Destination, Week, Day, Max_Duration, Max_Routes, Duration, Routes, 0, [], [Source]).
+    connected(Source, Destination, Week, Day, Max_Duration, Max_Routes, Duration, [Source], [], Routes).
 
-connected_helper(Source,Source,_,_,_,_,DurationAcc,RoutesAcc,DurationAcc,RoutesAcc,_):- !.
+connected(Source, Destination, Week, Day, Max_Duration, Max_Routes, Duration, Prev_Stations, Routes_So_Far, Routes):-
+    connected_testing(Source, Destination, Week, Day, Max_Duration, Max_Routes, Duration, Routes, 0, Routes_So_Far, [], Prev_Stations).
 
-connected_helper(Source, Destination, Week, Day, Max_Duration, Max_Routes, Duration, Routes, DurationAcc, RoutesAcc, PreviouslyVisited):-   
-    line(Transport,Line),
-    not(strike(Line, Week, Day)),
-    proper_connection(Source, X, Duration1, Transport),
-    not(member(X, PreviouslyVisited)),
-    append([X], PreviouslyVisited, PreviouslyVisited1),
+connected_testing(Source, Source, _, _, Max_Duration, Max_Routes, DurationAcc, RoutesAcc, DurationAcc, RoutesAcc, _, _):-
+    DurationAcc =< Max_Duration,
+    length(RoutesAcc, L),
+    L =< Max_Routes,
+    !.
+
+connected_testing(Source, Destination, Week, Day, Max_Duration, Max_Routes, Duration, Routes, DurationAcc, RoutesAcc, LineAcc, Prev_Stations):-
+    DurationAcc < Max_Duration,
+    length(RoutesAcc, L),
+    L < Max_Routes,
+    %get all possible stations
+    %notrace,
+    testing(Source, Stations),
+    %remove stations that are useless
+    filter_by_duration(Source, Stations, Max_Duration, FinalStations),
+    %remove stations that are on lines we previously visited
+    unique_elements(FinalStations, LineAcc, FinalStations1),
+    %remove stations that were previously visited
+    unique_elements_1(FinalStations1, Prev_Stations, FinalStations2),
+    %remove stations that might cause loops because of the ring roads
+    remove_ring(FinalStations2, LineAcc, CorrectedFinalStations),
+    %explore every member of the list
+    member(station_data(X, Duration1, Line), CorrectedFinalStations),
+    %continue with said member after getting its duration and line
+    line(Line, Mode),
+    not(strike(Mode, Week, Day)),
+    append(Prev_Stations, [Destination], New_Prev_Stations),
+    append(LineAcc, [Line], NewLineAcc),
     DurationAcc1 is DurationAcc + Duration1,
-    DurationAcc1 =< Max_Duration,
-    append_connection(Source, X, Duration1, Transport, RoutesAcc, RoutesAcc1),
-    length(RoutesAcc1, RouteCount),
-    RouteCount =< Max_Routes,
-    connected_helper(X, Destination, Week, Day, Max_Duration, Max_Routes, Duration, Routes, DurationAcc1, RoutesAcc1, PreviouslyVisited1).
+    append_connection(Source, X, Duration1, Line, RoutesAcc, RoutesAcc1),
+    %trace,
+    connected_testing(X, Destination, Week, Day, Max_Duration, Max_Routes, Duration, Routes, DurationAcc1, RoutesAcc1, NewLineAcc, New_Prev_Stations)
+    .
+
+unique_elements_1([], _, []).
+
+unique_elements_1([H|T], Prev_Stations, New_Prev_Stations):-
+    member(H, Prev_Stations),
+    !,
+    unique_elements_1(T, Prev_Stations, New_Prev_Stations).
+
+unique_elements_1([H|T], Prev_Stations, [H|New_Prev_Stations]):-
+    not(member(H, Prev_Stations)),
+    unique_elements_1(T, Prev_Stations, New_Prev_Stations).
+
+testing(Source, TestAns):-
+    findall(station_data(X,Y,Z),
+            proper_connection(Source, X, Y, Z),
+            TestAns1),
+    remove_repetitions(TestAns1, TestAns).
+
+unique_elements([], _, []).
+
+unique_elements([H|FirstList], SecondList, Unique) :-
+    H = station_data(_,_,X),
+    member(X, SecondList),
+    !,
+    unique_elements(FirstList, SecondList, Unique).
+
+unique_elements([H|FirstList], SecondList, [H|Unique]) :-
+    H = station_data(_,_,X),
+    \+ member(X, SecondList),
+    unique_elements(FirstList, SecondList, Unique).
+
+filter_by_duration(_, [], _, []).
+
+filter_by_duration(Source, [H|T], Max_Duration, [H|FinalStations]):-
+    H = station_data(_, Duration,_),
+    Duration =< Max_Duration,
+    filter_by_duration(Source, T, Max_Duration, FinalStations).
+
+filter_by_duration(Source, [H|T], Max_Duration, FinalStations):-  
+    H = station_data(_, Duration,_),
+    Duration > Max_Duration,
+    filter_by_duration(Source, T, Max_Duration, FinalStations).
+
+remove_ring([], _, []).
+remove_ring([H|T], Lines, FinalStations):-
+    H = station_data(_, _, Line),
+    remove_ring(T, Lines, FinalStations1),
+    (Line == s41 ->
+    (member(s42, Lines) ->
+        FinalStations = FinalStations1
+        ;
+        FinalStations = [H|FinalStations1])
+    ;
+    Line == s42 ->
+    (member(s41, Lines) ->
+        FinalStations = FinalStations1
+        ;
+        FinalStations = [H|FinalStations1])
+    ;
+    FinalStations = [H|FinalStations1]
+    ).
+
+
+%implementing Time Conversion -> mins_to_twentyfour_hr
+
+mins_to_twentyfour_hr(Minutes, TwentyFour_Hours, TwentyFour_Mins):-
+    ProperMinutes is Minutes mod 1440,
+    TwentyFour_Hours is ProperMinutes//60,
+    TwentyFour_Mins is ProperMinutes mod 60.
+
+%implementing Time Conversion -> twentyfour_hr_to_mins
+
+twentyfour_hr_to_mins(TwentyFour_Hours, TwentyFour_Mins, Minutes):-
+    Proper_TwentyFour_Hours is (TwentyFour_Hours mod 24) + TwentyFour_Mins//60,
+    Proper_TwentyFour_Mins is TwentyFour_Mins mod 60,
+    Minutes is (Proper_TwentyFour_Hours*60 + Proper_TwentyFour_Mins) mod 1440.
+
+%implementing Time Conversion -> slot_to_mins
+
+slot_to_mins(Slot_Num, Minutes):-
+    slot(Slot_Num, Start_Hour, Start_Minute),
+    twentyfour_hr_to_mins(Start_Hour, Start_Minute, TwentyFour_Mins),
+    Minutes is TwentyFour_Mins.
+
+%implementing the main predicate: travel_plan
+
+%notes:
+%Home_Stations is a list of all possible sources
+%The destination is always tegel
+%a journey consists of Week_Num, Week_Day, Start_Hour, Start_Minute, Total_Duration, Routes
+
+
+%in order to get Week_Num, Week_Day, and Max_Duration depending on the Group
+%group struct is (Week_Num, Week_Day, Max_Duration)
+/* info_getter(Week_Num, Week_Day, Slot, Group, Max_Duration):-
+    scheduled_slot(Week_Num, Week_Day, _, _, Group),
+    earliest_slot(Group, Week_Num, Week_Day, Slot),
+    slot_to_mins(Slot, Max_Duration).
+
+info_getter_list(Group, List):-
+    findall(group(Week_Num, Week_Day, Max_Duration),
+    info_getter(Week_Num, Week_Day, Slot, Group, Max_Duration),
+    List1),
+    remove_repetitions(List1, List).
+
+travel_plan(Starting_List, Group, Max_Duration, Max_Routes, Journeys):-
+    info_getter_list(Group, List),
+    travel_plan_helper(Starting_Stations_List, Group, Max_Duration, Max_Routes, Journeys, List).
+
+travel_plan_helper([], _, _, _, _, _).
+
+travel_plan_helper(Starting_Stations_List, Group, Max_Duration, Max_Routes, Journeys, [group(Week_Num, Week_Day, Max_Duration) | T], Answer):-
+    
+    travel_plan_helper_helper(Starting_Stations_List, Group, Max_Duration, Max_Routes, Week_Day, group(Week_Num, Week_Day, Max_Duration), Answer).
+
+travel_plan_helper_helper([],_,_,_,_,_,[]).
+
+travel_plan_helper_helper([H|T], Group, Max_Duration, Max_Routes, Day, group(Week_Num, Week_Day, Max_Duration), Answer):-
+    mins_to_twentyfour_hr(Max_Duration, TwentyFour_Hours, TwentyFour_Mins),
+    connected(H, tegel, Week_Num, Week_Day, Max_Duration, Max_Routes, Duration, Routes),
+    append(Answer, [journey(Week_Num, Week_Day, TwentyFour_Hours, TwentyFour_Mins, Duration, Routes)], New_Answer),
+    travel_plan_helper_helper(T, Group, Max_Duration, Max_Routes, Day, group(Week_Num, Week_Day, Max_Duration), New_Answer). */
+
+    
